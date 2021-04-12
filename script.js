2a58a3c41e55b62d40894c7a09db394794645b84
mdraju <md raju>
2021-04-13 02:15:14 +0600

    done

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ minusBtn.addEventListener("click", function() {
 })
 
 function getCrackerNumber(plus) {
-    const inputBox = document.getElementById("inputbox ").value;
+    const inputBox = document.getElementById("FirstClassInput ").value;
     const inputNumber = parseInt(inputBox);
     let newValue = inputNumber;
     if (plus == true) {
@@ -18,9 +18,51 @@ function getCrackerNumber(plus) {
         newValue = inputNumber - 1;
     }
 
-    document.getElementById("inputbox ").value = newValue;
+    document.getElementById("FirstClassInput ").value = newValue;
+    SubtotalAmount()
 
-    const Amount = newValue * 150;
-    console.log(Amount);
+}
+// ==========================================================================================================
+// =============================================================================================================
+document.getElementById("EconomePlus ").addEventListener("click", function() {
+    addEconomyCard(true);
+})
+document.getElementById("EconomeMinus ").addEventListener("click", function() {
+    addEconomyCard(false);
+})
 
+function addEconomyCard(add) {
+    const Economy = document.getElementById("EconomyInput ").value;
+    const EconomyNumber = parseInt(Economy);
+    let newValue = EconomyNumber;
+    if (add == true) {
+        newValue = EconomyNumber + 1;
+    }
+    if (add == false && newValue > 0) {
+        newValue = EconomyNumber - 1;
+    }
+    document.getElementById("EconomyInput ").value = newValue;
+
+    SubtotalAmount()
+
+
+}
+// =======================================================================================================
+// =========================================================================================================
+function SubtotalAmount() {
+    const FirstClassCricket = getInputValue("FirstClassInput ");
+    const EconomyCricket = getInputValue("EconomyInput ");
+    const SubtotalAmount = FirstClassCricket * 150 + EconomyCricket * 100;
+    document.getElementById("subtotal ").innerText = "$" + SubtotalAmount;
+    const vat = SubtotalAmount * 0.1;
+    document.getElementById("vat ").innerText = vat;
+    const total = SubtotalAmount + vat;
+    document.getElementById("total ").innerText = total;
+
+}
+
+function getInputValue(get) {
+    const cricket = document.getElementById(get).value;
+    const cricketNumber = parseInt(cricket);
+    return cricketNumber;
 }
